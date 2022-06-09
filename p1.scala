365ee5bc124397fadff4e1fd067381b3ae647272
MiguelAngelSirpa <91285270+MiguelAngelSirpa@users.noreply.github.com>
2022-06-09 09:21:58 +0200

    Add files via upload

---
+++ p1.scala
@@ -0,0 +1,25 @@
+object Tribonacci extends App{
+
+  def TriboRecursive(n: Int): Int = {
+    if      (n == 0) 0
+    if      (n == 1) 0
+    if      (n == 2) 1
+    else    TriboRecursive(n-1) + TriboRecursive(n-2)+TriboRecursive(n-3)
+  }
+
+  def TriboNormal(n: Int): Int = {
+    var a = -1
+    var b = 1
+    var c = 0
+    var d = 0
+    for (i <- 0 to n){
+      var buffer = a
+      a = b
+      b = c
+      c = d
+      d = buffer +d
+    }
+    d
+  } 
+
+}

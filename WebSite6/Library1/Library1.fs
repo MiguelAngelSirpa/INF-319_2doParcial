fbd8ddb70fbe15d135e1c4d9f10557c84484e998
MiguelAngelSirpa <91285270+MiguelAngelSirpa@users.noreply.github.com>
2022-06-10 20:32:02 +0200

    Add files via upload

---
+++ WebSite6/Library1/Library1.fs
@@ -0,0 +1,29 @@
+﻿namespace Library1
+module Calcu =
+    let tribonormal n =
+     let mutable a=(-1)
+     let mutable b=1
+     let mutable c=0
+     let mutable d=0
+     for i in 1..n do
+        printf "%d, " d
+        a<-b
+        b<-c
+        c<-d
+        d<-a+b+c
+                 
+    
+
+    let rec triboRecursivo n =
+        match n with
+        | 0 -> 0
+        | 1 ->0
+        | 2 ->1
+        | n ->triboRecursivo(n-1) + triboRecursivo(n-2)+triboRecursivo(n-3)
+
+    let triborec n=
+         for i in 1..n do
+            printf "%d, " (triboRecursivo i)
+
+type Class1() = 
+    member this.X = "F#"
